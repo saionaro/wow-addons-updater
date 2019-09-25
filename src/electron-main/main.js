aefc67ed5efd81734c262e33ee5233b75272b4b8
Saionaro <shibakow@gmail.com>
2019-09-25 13:46:09 +0300

    Hide useless menu

--- src/electron-main/main.js
+++ src/electron-main/main.js
@@ -1,49 +1,51 @@
-const {
-  app,
-  BrowserWindow,
-  ipcMain,
-  globalShortcut,
-} = require('electron');
+const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
 
-const DEV = process.env.NODE_ENV === 'development';
+const DEV = process.env.NODE_ENV === "development";
 
-const gatherAddonsWrapper = require('./actions/addons-gatherer.js');
-const downloadAddonWrapper = require('./actions/addon-loader.js');
-const chooseDirectoryWrapper = require('./actions/directory-chooser.js');
-const searchAddonWrapper = require('./actions/search-addon.js');
+const gatherAddonsWrapper = require("./actions/addons-gatherer.js");
+const downloadAddonWrapper = require("./actions/addon-loader.js");
+const chooseDirectoryWrapper = require("./actions/directory-chooser.js");
+const searchAddonWrapper = require("./actions/search-addon.js");
 
 const inst = {
   window: null,
-  tempPath: ''
+  tempPath: ""
 };
 
-const createWindow = (x, y) => new BrowserWindow({
-  width: x,
-  height: y,
-  titleBarStyle: 'hiddenInset',
-  webPreferences: {
-    nodeIntegration: true
-  }
-});
+const createWindow = (x, y) =>
+  new BrowserWindow({
+    width: x,
+    height: y,
+    titleBarStyle: "hiddenInset",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('ready', () => {
+app.on("ready", () => {
   inst.window = createWindow(800, 600);
-  inst.window.loadFile('./dist/index.html');
+  inst.window.loadFile("./dist/index.html");
+
+  inst.window.removeMenu();
+  app.dock.hide();
+  // inst.window.setMenuBarVisibility(false);
+
   if (DEV) {
     inst.window.webContents.openDevTools();
-    globalShortcut.register('f5', function() {
+    globalShortcut.register("f5", function() {
       inst.window.reload();
     });
   }
-  inst.tempPath = app.getPath('temp');
-  ipcMain.on('action/get-addons', gatherAddonsWrapper(inst));
-  ipcMain.on('action/get-addon-data', downloadAddonWrapper(inst));
-  ipcMain.on('action/choose-directory', chooseDirectoryWrapper(inst));
-  ipcMain.on('action/search-addon', searchAddonWrapper(inst));
+
+  inst.tempPath = app.getPath("temp");
+  ipcMain.on("action/get-addons", gatherAddonsWrapper(inst));
+  ipcMain.on("action/get-addon-data", downloadAddonWrapper(inst));
+  ipcMain.on("action/choose-directory", chooseDirectoryWrapper(inst));
+  ipcMain.on("action/search-addon", searchAddonWrapper(inst));
 });
